47a4bee1a842d61730b4b94310183940e71d6f3e
vdtrolf <vdt.rolf@xs4all.nl>
2022-04-03 08:52:56 +0200

    Static penguins

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const axios = require("axios");
 
-//import {fetch} from 'node-fetch';
-
 const args = process.argv.slice(2);
 
 // console.log(process.env);
@@ -10,30 +8,33 @@ args.forEach((arg) => {
   console.log(arg);
 });
 
-let islandSize = Number.parseInt(args[0], 10);
-if (!islandSize) islandSize = 40;
+let islandL = Number.parseInt(args[0], 10);
+if (!islandL) islandL = 120;
+let islandH = islandL / 3;
 
-console.log("Building an island of size " + islandSize);
+console.log("Building an island of size " + islandH + " * " + islandL);
 
 class Island {
-  constructor(size) {
-    this.size = size;
+  constructor(sizeH,sizeL) {
+    this.sizeH = sizeH;
+    this.sizeL = sizeL;
     this.territory = [];
+    this.penguins = [];
 
     // creating a matrix of land objects with a value of 0
-    for (let i = 0; i < size; i++) {
+    for (let h = 0; h < sizeH; h++) {
       let line = [];
-      for (let j = 0; j < size; j++) {
-        line.push(new Land(i, j));
+      for (let l = 0; l < sizeL; l++) {
+        line.push(new Land(h,l));
       }
       this.territory.push(line);
     }
 
     // first lines of land from line 1 to line size -1
-    let variation = islandSize / 5;
+    let variation = this.sizeL / 5;
     let start = variation,
       start2 = variation;
-    for (let j = 1; j < size - 1; j++) {
+    for (let h = 1; h < sizeH - 1; h++) {
       let rnd = Math.floor(Math.random() * 3 - 1 + start);
       start = rnd;
 
@@ -41,9 +42,9 @@ class Island {
       rnd2 = rnd2 < 0 ? 0 : rnd2;
       start2 = rnd2;
 
-      for (let i = rnd; i < size - rnd2; i++) {
-        if (this.territory[j][i]) {
-          this.territory[j][i].setLand(1);
+      for (let l = rnd; l < sizeL - rnd2; l++) {
+        if (this.territory[h][l]) {
+          this.territory[h][l].setLand(1);
         } else {
           console.log("const: cant set land for " + j + " " + i);
         }
@@ -51,9 +52,9 @@ class Island {
     }
 
     // sea borders on the upper and lower side
-    start = variation / 2;
-    start2 = variation / 2;
-    for (let j = 1; j < size - 1; j++) {
+    start = variation / 4;
+    start2 = variation / 4;
+    for (let l = 1; l < sizeL - 1; l++) {
       let rnd = Math.floor(Math.random() * 3 - 1 + start);
       start = rnd;
 
@@ -61,31 +62,39 @@ class Island {
       rnd2 = rnd2 < 0 ? 0 : rnd2;
       start2 = rnd2;
 
-      for (let i = 0; i < rnd; i++) {
-        this.territory[i][j].setLand(0);
+      for (let h = 0; h < rnd; h++) {
+        this.territory[h][l].setLand(0);
       }
 
-      for (let i = size - rnd2; i < size; i++) {
-        this.territory[i][j].setLand(0);
+      for (let h = sizeH - rnd2; h < sizeH; h++) {
+        this.territory[h][l].setLand(0);
       }
     }
 
-    for (let i = 0; i < size * 5; i++) {
-      // for (let i = 0; i < 2; i++) {
-      let xpos = Math.floor(Math.random() * size);
-      let ypos = Math.floor(Math.random() * size);
-      let land = this.territory[xpos][ypos];
-
-      // if (land) {
-      //   console.log("const: " + xpos + " " + ypos + " " + land.getType());
-      // } else {
-      //   console.log("const: " + xpos + " " + ypos + " -- no land");
-      // }
+    // Randomly elevate some terrain parts
+    for (let i = 0; i < sizeH * 30; i++) {
+      let hpos = Math.floor(Math.random() * sizeH);
+      let lpos = Math.floor(Math.random() * sizeL);
+      let land = this.territory[hpos][lpos];
 
       if (land && land.getType() !== 0) {
-        this.elev(land, xpos, ypos);
+        this.elev(land, hpos, lpos);
       }
     }
+    // randomly add some penguins
+    for (let i = 0; i < 10; i++) {
+      let hpos = Math.floor(Math.random() * sizeH);
+      let lpos = Math.floor(Math.random() * sizeL);
+      let land = this.territory[hpos][lpos];
+
+
+      if (land && land.getType() !== 0) {
+        let penguin = new Penguin(hpos,lpos,"toto","M",10);
+        land.addPenguin(penguin);
+        this.penguins.push(penguin);
+      }
+    }
+
   }
 
   getLandType(x, y) {
@@ -94,38 +103,80 @@ class Island {
     return land.getType();
   }
 
-  elev(land, xpos, ypos) {
+  hasPenguin(x, y) {
+    // console.log("-->" + x + " " + y);
+    const land = this.territory[x][y];
+    if (land.checkPenguin()) {
+      return true;
+    }
+    return false;
+  }
+
+
+  elev(land, hpos, lpos) {
     const height = land.getType() + 1;
     // console.log("elev: " + xcoord + " " + ycoord + " -> " + height);
-    for (let x = xpos - 1; x <= xpos + 1; x++) {
-      for (let y = ypos - 1; y <= ypos + 1; y++) {
-        if (x >= 0 && y >= 0) {
-          let lheight = this.territory[x][y].getType();
-          if (lheight > 0 && height - lheight > 1) {
-            // console.log("elev: upscaling " + x + " " + y + " " + lheight);
-            this.territory[x][y].setLand(lheight + 1);
+    for (let h = hpos - 1; h <= hpos + 1; h++) {
+      for (let l = lpos - 1; l <= lpos + 1; l++) {
+        if (h >= 0 && l >= 0 && h < this.sizeH && l < this.sizeL) {
+          try {
+            let lheight = this.territory[h][l].getType();
+            if (lheight > 0 && height - lheight > 1) {
+              // console.log("elev: upscaling " + x + " " + y + " " + lheight);
+              this.elev(this.territory[h][l],h,l);
+              //this.territory[h][l].setLand(lheight + 1);
+            }
+
+          } catch (error) {
+            console.error("No land at h=" + h + " / l" + l + " - " + error);
           }
         }
       }
     }
-    this.territory[xpos][ypos].setLand(height);
+    this.territory[hpos][lpos].setLand(height);
   }
 }
 
 class Land {
-  constructor(x, y) {
+  constructor(l, h) {
     // console.log(x + " " + y);
-    this.xcoord = x;
-    this.ycoord = y;
+    this.lpos = l;
+    this.hpos = h;
     this.type = 0;
+    this.penguin = null;
   }
 
   setLand(num) {
     this.type = num;
   }
 
+  addPenguin(penguin) {
+    console.log("New penguin at " + this.lpos + " " + this.hpos);
+    this.penguin = penguin;
+  }
+
+  checkPenguin(){
+    // console.log("Checking penguin at " + this.lpos + " " + this.hpos + " -> " + this.penguin);
+    return this.penguin;
+  }
+
   getType() {
     return this.type;
+  }
+}
+
+class Penguin {
+  constructor(l, h, name, gender, age) {
+    // console.log("New penguin " + l + " " + h);
+    this.lpos = l;
+    this.hpos = h;
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+  }
+
+  getName() {
+    return this.name;
   }
 }
 
@@ -135,43 +186,47 @@ const getWeather = () => {
       "http://api.weatherapi.com/v1/current.json?key=c28ff46234e64f52abe171323220204&q=London&aqi=no"
     )
     .then((response) => {
-      console.log(response.data.current);
+      console.log(response.data.current.condition.text);
     })
     .catch((error) => {
       console.log(error);
     });
 };
 
-const island = new Island(islandSize);
+const island = new Island(islandH,islandL);
 
 let linetop = "+";
-for (let j = 0; j < islandSize; j++) linetop += "-";
+for (let j = 0; j < islandL; j++) linetop += "-";
 console.log(linetop + "+");
 
-for (let i = 0; i < islandSize; i++) {
+for (let i = 0; i < islandH; i++) {
   let line = "|";
-  for (let j = 0; j < islandSize; j++) {
+  for (let j = 0; j < islandL; j++) {
+    if (island.hasPenguin(i,j)) {
+      line += "o";
+    } else {
     switch (island.getLandType(i, j)) {
       case 0:
         line += " ";
         break;
       case 1:
-        line += ".";
+        line += "░";
         break;
       case 2:
-        line += "^";
+        line += "▒";
         break;
       case 3:
-        line += "*";
+        line += "▓";
         break;
       case 4:
-        line += "#";
+        line += "█";
         break;
     }
+  }
   }
   console.log(line + "|");
 }
 
 console.log(linetop + "+");
 
-getWeather();
+// getWeather();
