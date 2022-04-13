6dd4826fff8af9f878f7117f9ad1d66065c23a51
play.js <git@gituser.com>
2022-04-13 18:42:41 +0200

    stdin

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const penguinReq = require("./penguin.js");
 const islandReq = require("./island.js");
 const landReq = require("./land.js");
 const axios = require("axios");
+const readline = require('readline')
+const strings = require('./strings')
+//const colors = require('colors/safe')
+
+//colors.enable()
 
 let Penguin = penguinReq.Penguin;
 let Island = islandReq.Island;
@@ -28,7 +33,7 @@ args.forEach((arg) => {
 });
 
 let islandL = Number.parseInt(args[0], 10);
-if (!islandL) islandL = 90;
+if (!islandL) islandL = 40;
 let islandH = islandL / 2;
 
 let mode = Number.parseInt(args[1], 10);
@@ -45,6 +50,28 @@ let island = new Island(islandH,islandL);
 if (debug) {
   console.log(island.getAscii(mode,islandH,islandL));
 }
+
+const print = (msg) => {
+  console.log(msg)
+}
+
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+})
+
+rl.on('line', (line) => {
+  print('-----------')
+  var input = line.replace(/\0/g, '')
+  console.log(island.getAscii(mode,islandH,islandL));
+  
+  console.log("tata");
+})
+
+print(strings.hello)
+print('')
+process.stdout.write(strings.prompt)
 
 if (listen) {
 
@@ -66,10 +93,15 @@ if (listen) {
   });
 
   app.listen(port, () => {
-    console.log(`Little island listening at port: ${port}`);
+    // print(`Little island listening at port: ${port}`);
   });
 }
+
+
 
 setInterval(() => {
     island.movePenguins();
 }, 1000);
+
+
+
---
+++ strings.js
@@ -0,0 +1,11 @@
+const hello = 'Tap on the console and insert a command.'
+const prompt = 'Command:'
+const result = 'IP address:'
+const error = 'Received error with code:'
+
+module.exports = {
+  hello: hello,
+  prompt: prompt,
+  result: result,
+  error: error
+}
