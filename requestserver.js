77959a149bc040986434733355d474078d02ccff
vdtrolf <vdt.rolf@xs4all.nl>
2022-06-06 22:16:30 +0200

    No params

--- requestserver.js
+++ requestserver.js
@@ -79,7 +79,7 @@ const createResponse = (url,params,sessionId) => {
   //let sId = 0;
   //let iId = 0;
 
-  console.log("in createResponse url: >" + url + "< params: >" + params.renew + "< sessionId: >" + sessionId + "<")
+  console.log("in createResponse url: >" + url + "< sessionId: >" + sessionId + "<")
 
 
   if (sessionId > 0) {
