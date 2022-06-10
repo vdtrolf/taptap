8de14955fb1e7f8da5b8b8cf1ef7aba109a34bb6
vdtrolf <vdt.rolf@xs4all.nl>
2022-06-10 08:14:58 +0200

    No express

--- index_express.js
+++ index_express.js
@@ -12,50 +12,50 @@ let requestcounter = 0;
 
 if (startExpress) {
 
-  // const port = 3001;
-  // let app = null;
-  //
-  // const express = require('express');
-  // const cors = require('cors');
-  //
-  // app = express();
-  // app.use(cors())
-  // app.use(express.json());
-  // app.use(
-  //   express.urlencoded({
-  //     extended: true,
-  //   })
-  // );
-  //
-  // try {
-  //   app.get('/*', (req, res) => {
-  //
-  //     let sessionId = Number.parseInt(req.query.sessionId,10);
-  //
-  //     if (debug ) {
-  //       timeTag = new Date().getTime() - baseTime;
-  //       console.log(timeTag + " index.js : Processing " + req.path + " for session " + sessionId + " renew = "+ req.query.renew)
-  //     };
-  //
-  //     return res.json(createResponse(req.path,req.query, sessionId));
-  //   });
-  //
-  //   app.listen(port, () => {
-  //     console.log(`index.js : Little island listening at port: ${port}`);
-  //   });
-  //
-  //   app.on('error', (e) => {
-  //     console.log("index.js : app error " + e.code);
-  //   });
-  //
-  //   process.on('error', (e) => {
-  //     console.log("index.js : process error " + e.code);
-  //   });
-  //
-  // } catch(error) {
-  //    console.error("index.js : problem " + error);
-  // }
-  //
+  const port = 3001;
+  let app = null;
+
+  const express = require('express');
+  const cors = require('cors');
+
+  app = express();
+  app.use(cors())
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+
+  try {
+    app.get('/*', (req, res) => {
+
+      let sessionId = Number.parseInt(req.query.sessionId,10);
+
+      if (debug ) {
+        timeTag = new Date().getTime() - baseTime;
+        console.log(timeTag + " index.js : Processing " + req.path + " for session " + sessionId + " renew = "+ req.query.renew)
+      };
+
+      return res.json(createResponse(req.path,req.query, sessionId));
+    });
+
+    app.listen(port, () => {
+      console.log(`index.js : Little island listening at port: ${port}`);
+    });
+
+    app.on('error', (e) => {
+      console.log("index.js : app error " + e.code);
+    });
+
+    process.on('error', (e) => {
+      console.log("index.js : process error " + e.code);
+    });
+
+  } catch(error) {
+     console.error("index.js : problem " + error);
+  }
+
 } else {
   console.log('index.js : Little island function listening on Lambda');
 }
