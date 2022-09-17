19b76123187e64ea83de8250e9fc22bdcf8882e8
vdtrolf <vdt.rolf@xs4all.nl>
2022-09-17 12:46:31 +0200

    JSON parse

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ if (local) {
         "response is now : " + JSON.stringify(aresponse)
       );
 
-      return { running: running }; // aresponse;
+      return JSON.parse({ running: running }); // aresponse;
     } else {
       let sessionId = 0;
       let counterId = 0;
