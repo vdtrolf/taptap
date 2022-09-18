7d5f923109c40ac15f50bd53155825b285990480
Rolf van der Tang <vdt.rolf@xs4all.nl>
2022-09-18 17:59:42 +0200

    Extra revival

--- stephelper.js
+++ stephelper.js
@@ -1,49 +1,69 @@
-const AWS = require("aws-sdk");
-const stepFunctions = new AWS.StepFunctions();
+let executionName = "taptap" + new Date().getTime();
 
-const startStateSteps = () => {
-  const params = {
-    stateMachineArn:
-      "arn:aws:states:us-east-1:778787439795:stateMachine:Taptap_state_engine",
-    input: "",
-    name: "ExecutionLambda",
+function startStateSteps() {
+  var AWS = require('aws-sdk');
+
+  var stepfunctions = new AWS.StepFunctions();
+
+  var params = {
+    stateMachineArn: 'arn:aws:states:us-east-1:778787439795:stateMachine:Taptap_state_engine',
+    input: '{"OrderID":266}',
+    name: '00002'
   };
 
-  console.log("==========> Going to start engine");
+  stepfunctions.startExecution(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+}
 
-  try {
-    stepFunctions.startExecution(params, (err, data) => {
-      if (err) {
-        console.log(err);
 
-        const response = {
-          statusCode: 500,
-          body: JSON.stringify({
-            message: "There was an error",
-          }),
-        };
+// const startStateSteps = () => {
+//   const params = {
+//     stateMachineArn:
+//       "arn:aws:states:us-east-1:778787439795:stateMachine:Taptap_state_engine",
+//     input: "{}",
+//     name: executionName
+//   };
 
-        return response;
-        // callback(null, response);
-      } else {
-        console.log(data);
+//   console.log("==========> Going to start engine at " + executionName);
 
-        const response = {
-          statusCode: 200,
-          body: JSON.stringify({
-            message: "Step function worked",
-          }),
-        };
+//   try {
+//     let result = stepFunctions.startExecution(params, function(err, data) {
+//       if (err) {
+//         console.log(err);
 
-        return response;
+//         // const response = {
+//         //   statusCode: 500,
+//         //   body: JSON.stringify({
+//         //     message: "There was an error",
+//         //   }),
+//         // };
 
-        // callback(null, response);
-      }
-    });
-  } catch {
-    console.err("Error while starting step", err);
-  }
-};
+//         // return response;
+        
+//       } else {
+//         console.log(data);
+
+//         // const response = {
+//         //   statusCode: 200,
+//         //   body: JSON.stringify({
+//         //     message: "Step function worked",
+//         //   }),
+//         // };
+
+//         // return response;
+
+//       }
+//     });
+//     console.log("==========> result <======");
+//     console.dir(result);
+//     console.log("==========> result <======");
+
+//   } catch {
+//     console.err("Error while starting step", err);
+//   }
+// };
 
 // now we export the class, so other modules can create Penguin objects
 module.exports = {
