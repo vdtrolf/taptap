952db80a0d6e37120a1f9a45ca7f7265da74c2ac
vdtrolf <vdt.rolf@xs4all.nl>
2022-09-18 08:23:11 +0200

    stepHelper

---
+++ stephelper.js
@@ -0,0 +1,40 @@
+const AWS = require("aws-sdk");
+const stepFunctions = new AWS.StepFunctions();
+
+const startStateSteps = () => {
+  const params = {
+    stateMachineArn:
+      "arn:aws:states:us-east-1:778787439795:stateMachine:Taptap_state_engine",
+    input: "",
+    name: "execution lambda",
+  };
+
+  stepFunctions.startExecution(params, (err, data) => {
+    if (err) {
+      console.log(err);
+
+      const response = {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: "There was an error",
+        }),
+      };
+      callback(null, response);
+    } else {
+      console.log(data);
+
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "Step function worked",
+        }),
+      };
+      callback(null, response);
+    }
+  });
+};
+
+// now we export the class, so other modules can create Penguin objects
+module.exports = {
+  startStateSteps,
+};
